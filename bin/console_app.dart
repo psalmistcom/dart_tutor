87495d6a3e083d352f7ef35d5114a28e3071e1db
David Adewale <dadewale9@gmail.com>
2022-09-15 09:55:35 +0100

    hello

---
+++ bin/console_app.dart
@@ -0,0 +1,24 @@
+// void main(List<String> arguments) {
+//   print('Hello world!');
+// }
+// import 'dart:ffi';
+
+void main(List<String> arguments) {
+  // String myString = "This is my first String is";
+  // print(myString);
+
+  // print(myString.contains("is"));
+
+  // int myInteger = 44;
+  // double myDouble = 23.5;
+
+  // print(myInteger.toString());
+
+  // final myFinal = "This is a constant constant";
+  // print(myFinal.toString());
+
+  var multipleVariable = "This is a multipleVariable";
+  multipleVariable = "This is a multipleVariable";
+
+  print(multipleVariable.toString());
+}
